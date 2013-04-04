891e9dc9fd0d43f77682e927bb7cf88ba9ca1b4d
Matt Drewitt <mattdrewitt@gmail.com>
2013-04-04 16:52:05 -0400

    Fixed Library configs and added 64 bit

--- SocketLibrary/ClientTesting/ClientMain.cpp
+++ SocketLibrary/ClientTesting/ClientMain.cpp
@@ -1,8 +1,17 @@
 #include <UDPClient.hpp>
+#include <TCPClient.hpp>
 
 using namespace std;
 
+#if defined (_DEBUG) &&  !defined(_DLL)
 #pragma comment (lib, "socketLibrary-mt-sgd.lib")
+#elif !defined (_DEBUG) &&  !defined(_DLL)
+#pragma comment (lib, "socketLibrary-mt-s.lib")
+#elif defined (_DEBUG) &&  defined(_DLL)
+#pragma comment (lib, "socketLibrary-mt-gd.lib")
+#elif !defined (_DEBUG) &&  defined(_DLL)
+#pragma comment (lib, "socketLibrary-mt.lib")
+#endif
 int main() {
 	UDPClient udpclient("127.0.0.1", 49154);
 	udpclient.connect();
--- SocketLibrary/ServerTesting/ServerMain.cpp
+++ SocketLibrary/ServerTesting/ServerMain.cpp
@@ -1,9 +1,18 @@
 #include <UDPServer.hpp>
 #include <TCPServer.hpp>
 
+#if defined (_DEBUG) &&  !defined(_DLL)
+#pragma comment (lib, "socketLibrary-mt-sgd.lib")
+#elif !defined (_DEBUG) &&  !defined(_DLL)
+#pragma comment (lib, "socketLibrary-mt-s.lib")
+#elif defined (_DEBUG) &&  defined(_DLL)
+#pragma comment (lib, "socketLibrary-mt-gd.lib")
+#elif !defined (_DEBUG) &&  defined(_DLL)
+#pragma comment (lib, "socketLibrary-mt.lib")
+#endif
+
 using namespace std;
 
-#pragma comment (lib, "socketLibrary-mt-sgd.lib")
 int main() {
 	UDPServer udpserver("127.0.0.1", 49154);
 	udpserver.Accept();
