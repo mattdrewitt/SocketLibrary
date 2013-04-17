008840f342619be52d47d7cec51bc045fb6c2c8c
Kayla Boyer <boyer55@hotmail.com>
2013-04-17 19:13:12 -0400

    dealer class

--- SocketLibrary/ServerTesting/Dealer.hpp
+++ SocketLibrary/ServerTesting/Dealer.hpp
@@ -1,6 +1,36 @@
 
-
+#include <string>
+#include <map>
 
 //ok so...should the objects go here or client side????
 //dealer, player 
 //numberdata --card, value, type
+
+//Simply a data class.... 
+class NumberData {
+private:
+	std::string card;
+	unsigned int value;
+	bool isFaceCard;
+public:
+	NumberData(std::string& c, unsigned int& val, bool& isFace) : card(c), value(val), isFaceCard(isFace){}
+};
+
+
+class Player {
+private:
+	double  playerBet;
+	typedef NumberData Cards[3];  //each player never has more than 3 cards. 
+	//std::map<unsigned int, Cards> playerHands; //each player may have more than 1 hand if they "split" 
+public:
+	
+};
+
+
+//Want the dealer to be like a singleton....not quite sure how to ensure that. 
+class Dealer {
+private:
+
+public:
+	Dealer() {}
+};
